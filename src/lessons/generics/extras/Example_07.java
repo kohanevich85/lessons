6bb75934f9bcc17532e2b692dbc2c1becbfd2388
Denis <kohanevich85@mail.com>
2016-03-30 00:31:01 +0300

    update generics

--- src/lessons/generics/extras/Example_07.java
+++ src/lessons/generics/extras/Example_07.java
@@ -1,7 +1,58 @@
 package lessons.generics.extras;
 
 /**
- * Created by Denis on 3/30/2016.
+ * Return child type from parent
  */
 public class Example_07 {
+    public static void main(String[] args) {
+        AbstractAction<ConcreteActionA> actionA = new ConcreteActionA();
+        actionA.execute();
+        actionA.and().pipelineA().with().pipelineA();
+
+        AbstractAction<ConcreteActionB> actionB = new ConcreteActionB();
+        actionB.execute();
+        actionB.and().pipelineB().with().pipelineB();
+
+        AbstractAction<ConcreteActionB> actionC = null;
+    }
+}
+
+interface Action {
+    void execute();
+}
+
+abstract class AbstractAction<T extends AbstractAction<T>> implements Action {
+    @SuppressWarnings("unchecked")
+    public T and() {
+        return (T) this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T with() {
+        return (T) this;
+    }
+}
+
+class ConcreteActionA extends AbstractAction<ConcreteActionA> {
+    @Override
+    public void execute() {
+        System.out.println("concrete action A");
+    }
+
+    ConcreteActionA pipelineA() {
+        System.out.println("in pipelineA ...");
+        return this;
+    }
+}
+
+class ConcreteActionB extends AbstractAction<ConcreteActionB> {
+    @Override
+    public void execute() {
+        System.out.println("concrete action B");
+    }
+
+    ConcreteActionB pipelineB() {
+        System.out.println("in pipelineA B ...");
+        return this;
+    }
 }
